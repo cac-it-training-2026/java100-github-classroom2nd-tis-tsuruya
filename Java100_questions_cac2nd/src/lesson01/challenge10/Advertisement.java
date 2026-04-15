d51f112c23adf899a94a5aa90325c467c10176b4
tsuruyas <tsuruyas@C010423394.zipangu.cac.co.jp>
2026-04-15 09:52:58 +0900

    lesson01のchallenge10

---
+++ Java100_questions_cac2nd/src/lesson01/challenge10/Advertisement.java
@@ -0,0 +1,15 @@
+package lesson01.challenge10;
+
+public class Advertisement {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("マカロン屋で働ける方を募集しています！\n");
+		System.out.println("-----募集要項-----\n");
+		System.out.println("①勤務時間 AM 9：00～PM 17：30(1時間休憩)");
+		System.out.println("②時給 \\1000");
+		System.out.println("③待遇\n・制服貸与あり\n・交通費全額支給\n");
+
+	}
+
+}
